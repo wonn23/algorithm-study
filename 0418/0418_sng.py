7532ddda1f536c5eda0826884ea2a87434565468
Lee SH <snghyun331@gmail.com>
2023-04-18 16:51:44 +0900

    분할법 수정

--- 0418/0418_sng.py
+++ 0418/0418_sng.py
@@ -26,16 +26,16 @@ print(total)
 
 
 #################### 2231 분해합
-N = int(input())
+N = int(input())  
 
-for i in range(N):
-    sum_inx = 0
-    for j in str(i):
-        sum_inx += int(j)
-    if int(N) == sum_inx + i:
+for i in range(1, N+1):   # i = N의 모든 생성자 (i = 198일 때)
+    num = sum((map(int, str(i))))  # i의 각 자릿수를 더함 (num = 1+9+8 = 18)
+    num_sum = i + num  # 분해합 = 생성자 + 각 자릿수의 합  (num_sum = 198 + 18 = 216)
+    # 처음으로 분해합과 입력값이 같을때가 가장 작은 생성자
+    if num_sum == N:  # if 216 == 126
         print(i)
         break
-else:
+else:  # for반복문이 다 돈 후에도 num_sum == N에 해당하는 i값을 못찾으면
     print(0)
     
     
